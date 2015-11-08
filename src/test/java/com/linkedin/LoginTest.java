fe923140a13729283d559e4c4ec4110b1720fa1c
karabl <rylovnikov.a@gmail.com>
2015-11-08 12:56:59 +0200

    Hello

--- src/test/java/com/linkedin/LoginTest.java
+++ src/test/java/com/linkedin/LoginTest.java
@@ -8,7 +8,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-import java.util.concurrent.TimeUnit; //fgfgfgfgfg
+import java.util.concurrent.TimeUnit; //fgfgfgfgfgdrgrgttg
 
 public class LoginTest {
     @Test
